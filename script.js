dd657fa60c2bde85cca8f095fb7c22abef2ffd93
Phantom <fay.ashbro@gmail.com>
2020-09-15 19:57:30 -0400

    icons added five day forecast added

--- script.js
+++ script.js
@@ -22,6 +22,7 @@
     var submitBtn = $("#submitBtn");
     
     var cityList = [];
+    console.log(cityList);
     var savedCitiesEl = $("#savedCities");
     // console.log(cityList);
     var cityNameEl = $("#cityName");
@@ -29,6 +30,9 @@
     var humidityEl = $("#humidity");
     var windEl = $("#wind");
     var uvEl = $("#uv");
+    var fiveDayEl = $("#fiveDayForecast");
+    var iconId; 
+    var currentIconId = $("#currentIconId");
     
    
     var cityCount = 0;
@@ -75,20 +79,25 @@
         var cityName = $("#searchInput").val();
         
         // console.log(cityName);
-        var lat 
-        var lon 
-        var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=98291c34b7371fc6b13373019411c995";
-        var fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q="+ cityName + "&cnt=5" + "&units=imperial&appid=98291c34b7371fc6b13373019411c995";
-        
+      
+        var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=98291c34b7371fc6b13373019411c995";        
 
 
         $.ajax({
             url: weatherURL,
             method: "GET",
         }).then(function (response) {
-            // console.log(response);
+             console.log(response);
             // console.log(response.main.name); wont work 
-            cityNameEl.text(cityName);
+            iconId = response.weather[0].icon; 
+            console.log(response.weather[0].icon);
+
+            var iconURL = "http://openweathermap.org/img/wn/" + iconId + "@2x.png"
+            // var icon = $("<img>");
+            currentIconId.attr("src", iconURL);
+            
+            cityNameEl.text(cityName + " ");
+
             // console.log(response.main.temp);
             // var tempK = response.main.temp;
             // var temp = Math.floor((tempK - 273.15)*1.800+32.00);
@@ -101,67 +110,87 @@
             // console.log(response.weather.icon); wont work 
             //Lattitude and Longitude
             
-            
+       
 
             lat = response.coord.lat;
             lon = response.coord.lon;
-            var uvURL = "http://api.openweathermap.org/data/2.5/uvi?appid=98291c34b7371fc6b13373019411c995" + "&lat=" +  lat + "&lon=" +lon;
+            var fiveDayURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + lon +"&exclude=minutely,hourly&appid=98291c34b7371fc6b13373019411c995";
+
 
             $.ajax({
-                url: uvURL, 
+                url: fiveDayURL, 
                 method: "GET",
             }).then(function (response) {
-                // console.log(response);
-                uvEl.text("UV Index: " + response.value);
-                if(response.value <= 2 ){
+                 console.log(response);
+                
+                uvEl.text("UV Index: " + response.current.uvi);
+                if(response.current.uvi <= 2 ){
                     uvEl.addClass("favorable")
-                }else if (response.value > 2 && response.value <7 ){
-                    uvEl.addClass("moderate")
-                }else if (response.value > 7 ){
+                }else if (response.current.uvi > 7 ){
                     uvEl.addClass("severe")
+                }else{
+                    uvEl.addClass("moderate")
                 }
-            })
+                for (var i = 0; i < 4; i++){
+                    var div = $("<div>");
+                    div.addClass("fiveDayForecast")
+
+                    //Weather Icon 
+                    iconId = response.daily[i].weather[0].icon; 
+                    console.log(response.daily[i].weather[0].icon);
+                    var iconURL = "http://openweathermap.org/img/wn/" + iconId + "@2x.png"
+                    var icon = $("<img>");
+                    icon.attr("src", iconURL);
+                    
+                
+                    var pTemp = $("<p>");
+                // //temp convert to farenheight
+                    pTemp.text("Temp: " + Math.floor((response.daily[i].temp.max - 273.15)*1.800+32.00))
+                
+                // //humidity 
+                    var pHumid = $("<p>");
+                
+                    pHumid.text("Humidity: " +response.daily[i].humidity);
+
+                    fiveDayEl.append(div);
+                    div.append(icon);
+                    div.append(pTemp);
+                    div.append(pHumid);
+
+                };
+            });
             
             savedCitiesEl.innerHTML= "";
            //Create p tags for new cities 
-        for (var i = 0; i < cityList.length; i++){
-            var city = cityList[i];
-            console.log(city);
-            var btnCity =  $("<button>");
-            btnCity.text(city);
-            savedCitiesEl.append(btnCity);   
-        }
+            for (var i = 0; i < cityList.length; i++){
+                var city = cityList[i];
+                console.log(city);
+                var btnCity =  $("<button>");
+                btnCity.text(city);
+                savedCitiesEl.append(btnCity);   
+            }
+
+           
+            cityList.push(cityName);
+            storeCity();
+            cityCount++;
+
             
         })
 
-        cityList.push(cityName);
-        storeCity();
-        cityCount++;
+       
 
         
+         //date ???   
+            // //icon needs to be another call 
+            // response.list[i].weather.icon
        
         // http://openweathermap.org/img/w/ + currentIcon + ".png"
         //add the citys to list 
        
 
 
-
-
-        // $.ajax({
-        //     url: fiveDayURL,
-        //     method: "GET",
-        // }).then(function (response) {
-        //     console.log(response);
-        //     //date 
-            
-        //     //icon 
-        //     response.list[i].weather.icon
-        //     //temp convert to farenheight
-        //     response.list[i].temp.day 
-        //     //humidity 
-        //     response.list[i].humidity
-        // })
-
+        
     } )
 
 
